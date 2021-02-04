4f31421552a40ca2bcb84bfdec7837194a0d1ad6
George Jensen <george@gcjensen.uk>
2021-02-04 10:05:12 +0000

    Change generic type in Day3 to Wire

--- app/src/main/java/com/gcjensen/aoc2019/day3/Day3.java
+++ app/src/main/java/com/gcjensen/aoc2019/day3/Day3.java
@@ -6,20 +6,18 @@ import java.util.stream.Stream;
 
 import com.gcjensen.aoc2019.Day;
 
-public class Day3 extends Day<String> {
+public class Day3 extends Day<Wire> {
     public Day3() {
         super(3);
     }
 
     @Override
-    public List<String> parseInput(Stream<String> input) {
-        return input.collect(Collectors.toList());
+    public List<Wire> parseInput(Stream<String> input) {
+        return input.map(Wire::parse).collect(Collectors.toList());
     }
 
     @Override
-    public Integer solvePart1(List<String> input) {
-        var wires = input.stream().map(Wire::parse).collect(Collectors.toList());
-
+    public Integer solvePart1(List<Wire> wires) {
         return Grid.withWires(wires).getWireOverlaps().stream()
             .map(Grid::distanceFromOrigin)
             .min(Integer::compareTo)
@@ -28,7 +26,7 @@ public class Day3 extends Day<String> {
 
 
     @Override
-    public Integer solvePart2(List<String> input) {
+    public Integer solvePart2(List<Wire> input) {
         return null;
     }
 }
--- app/src/test/java/com/gcjensen/aoc2019/day3/Day3Test.java
+++ app/src/test/java/com/gcjensen/aoc2019/day3/Day3Test.java
@@ -7,13 +7,13 @@ import org.junit.Test;
 import com.gcjensen.aoc2019.Day;
 
 public class Day3Test {
-    Day<String> day3 = new Day3();
+    Day<Wire> day3 = new Day3();
 
-    private final List<String> testInput = List.of(
-        "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
-        "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+    private final List<Wire> testInput = List.of(
+        Wire.parse("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"),
+        Wire.parse("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
     );
-    private final List<String> actualInput = day3.parseInput();
+    private final List<Wire> actualInput = day3.parseInput();
 
     @Test public void testPart1() {
         assertEquals(135, day3.solvePart1(testInput));
